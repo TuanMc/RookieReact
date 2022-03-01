4fcff9a5ae51e8a3cc086c9bec64abd3c5e4e795
tinySharkk <dongocthanh40@gmail.com>
2022-03-01 22:06:10 +0700

    exercise 1

---
+++ react-state-example/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import Home from './components/Home';
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+
+//const bootcamp : 'Rookies';
+
+class App extends Component {
+
+  state = {
+    bootcamp : 'Rookies'
+  }
+  render(){
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+
+    <Home Name = {this.state.bootcamp}/>
+    </React.Fragment>
+ 
+  );
+}
+}
+
+export default App;
---
+++ react-state-example/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(props) {
+    return (
+        <div>
+            <h2>
+                Welcome to {props.Name}
+            </h2>
+        </div>
+    )
+}
+
+export default Home;
